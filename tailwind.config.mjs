c5b0d6643904939fa773db024ab50eaccf2d809b
Taufik Nurhidayat <fiik346@gmail.com>
2024-02-09 10:27:09 +0700

    add more components

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,5 +4,42 @@ export default {
   theme: {
     extend: {},
   },
+  daisyui: {
+    themes: [
+      {
+        light: {
+          primary: "#020617",
+          "primary-content": "#ffffff",
+          secondary: "#64748b",
+          "secondary-content": "#ffffff",
+          accent: "#67CBA0",
+          neutral: "#181A2A",
+          "base-100": "#ffffff",
+          info: "#059669",
+          success: "#60a5fa",
+          warning: "#fbbf24",
+          error: "#dc2626",
+          "--rounded-box": "0.5rem",
+          "--rounded-btn": "0.25rem",
+        },
+      },
+      // {
+      //   dark: {
+      //     "color-scheme": "dark",
+      //     primary: "#020617",
+      //     secondary: "oklch(74.8% 0.26 342.55)",
+      //     accent: "oklch(74.51% 0.167 183.61)",
+      //     neutral: "#2a323c",
+      //     "neutral-content": "#A6ADBB",
+      //     "base-100": "#1d232a",
+      //     "base-200": "#191e24",
+      //     "base-300": "#15191e",
+      //     "base-content": "#A6ADBB",
+      //     "--rounded-box": "0.5rem",
+      //     "--rounded-btn": "0.25rem",
+      //   },
+      // },
+    ],
+  },
   plugins: [require("daisyui")],
 };
