ad82a92bbd92b449d5577256611e52f0a8820995
Taufik Nurhidayat <fiik346@gmail.com>
2024-06-30 08:31:48 +0700

    update config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import markdoc from "@astrojs/markdoc";
+
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), markdoc()],
+  output: "hybrid",
+  adapter: node({
+    mode: "standalone"
+  })
 });
-
