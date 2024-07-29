3479a207c9b153410109599a5151199d3b019941
Taufik Nurhidayat <fiik346@gmail.com>
2024-07-29 07:39:54 +0700

    chore: add cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,13 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import markdoc from "@astrojs/markdoc";
+// import node from "@astrojs/node";
 
-import node from "@astrojs/node";
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), markdoc()],
   output: "hybrid",
-  adapter: node({
-    mode: "standalone"
-  })
+  adapter: cloudflare()
 });
