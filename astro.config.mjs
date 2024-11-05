809c7cee5d87e53645a78a04784b9b58c91e2aff
Taufik Nurhidayat <fiik346@gmail.com>
2024-11-05 16:53:01 +0700

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,9 +6,13 @@ import cloudflare from "@astrojs/cloudflare";
 
 import playformCompress from "@playform/compress";
 
+import node from "@astrojs/node";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), markdoc(), playformCompress()],
   output: "hybrid",
-  adapter: cloudflare()
+  adapter: node({
+    mode: "standalone"
+  })
 });
