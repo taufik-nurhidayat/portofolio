5481b76b2881ae2cd02c9794c011f147d0f16d56
Taufik Nurhidayat <fiik346@gmail.com>
2024-11-05 16:54:42 +0700

    fix: node

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import markdoc from "@astrojs/markdoc";
 
-import cloudflare from "@astrojs/cloudflare";
 
 import playformCompress from "@playform/compress";
 
